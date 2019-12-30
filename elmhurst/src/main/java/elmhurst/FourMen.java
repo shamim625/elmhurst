f9746c6520690f8c4badaf3e4f65a3e6d3374ae6
Shamim Ahmed <ahmedshamim725@yahoo.com>
2019-12-30 18:25:27 -0500

    adding calculation

---
+++ elmhurst/src/main/java/elmhurst/FourMen.java
@@ -0,0 +1,14 @@
+package elmhurst;
+
+public class FourMen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=7;
+		int b=25;
+		double c=b/a;
+System.out.println(c);
+	}
+
+}
--- elmhurst/src/test/java/elmhurst/shamim.java
+++ elmhurst/src/test/java/elmhurst/shamim.java
@@ -5,6 +5,7 @@ public class shamim {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("first git push");
+	
 	}
-
+	
 }
