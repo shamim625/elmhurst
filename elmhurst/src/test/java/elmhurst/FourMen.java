7fe38268bc69720ab53d30642ac9c30e96ee38d9
Shamim Ahmed <ahmedshamim725@yahoo.com>
2020-01-04 10:17:38 -0500

    update

--- elmhurst/src/test/java/elmhurst/FourMen.java
+++ elmhurst/src/test/java/elmhurst/FourMen.java
@@ -5,8 +5,8 @@ public class FourMen {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int a=7;
-		int b=25;
+		double a=7;
+		double b=25;
 		double c=b/a;
 System.out.println(c);
 	}
