93088d808d8768728dae0e448637c351253a0be8
Shamim Ahmed <ahmedshamim725@yahoo.com>
2020-01-09 14:37:46 -0500

    Pushing project for group B with extra sample of amazon

---
+++ elmhurst/src/test/java/elmhurst/Amazon.java
@@ -0,0 +1,39 @@
+package elmhurst;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Amazon {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		
+		driver.get("https://www.amazon.com/");
+		System.out.println(driver.getTitle());
+		driver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]/span[1]")).click();
+		driver.findElement(By.name("email")).sendKeys("ahmedshamim725@yahoo.com");
+		driver.findElement(By.xpath("//*[@id=\"continue\"]")).click();
+		driver.findElement(By.name("password")).sendKeys("Tasfia68");
+		driver.findElement(By.id("signInSubmit")).click();
+		System.out.println(driver.getTitle());
+		//driver.findElement(By.xpath("//*[@id=\"nav-cart\"]")).click();
+		//driver.findElement(By.id("nav-cart")).click();
+		driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).sendKeys("winter hat");
+		driver.findElement(By.xpath("//*[@id=\"nav-search\"]/form/div[2]/div/input")).click();
+		
+		driver.findElement(By.id("nav-hamburger-menu")).click();
+		driver.findElement(By.xpath("//*[@id=\"hmenu-content\"]/ul[35]/li[47]/a")).click();
+		//System.out.println(driver.getTitle());
+		
+		//driver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]/span[1]"))
+	
+		//driver.findElement(By.id("nav-item-signOut")).click();
+
+	}
+
+}
---
+++ elmhurst/src/test/java/elmhurst/PHPTravel.java
@@ -0,0 +1,32 @@
+package elmhurst;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PHPTravel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://newtours.demoaut.com/");
+		System.out.println(driver.getTitle());
+		driver.findElement(By.name("userName")).sendKeys("user@phptravels.com");
+		driver.findElement(By.name("password")).sendKeys("demouser");
+		driver.findElement(By.name("login")).click();
+		System.out.println(driver.getTitle());
+		driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/b/font/input[2]")).sendKeys("one way");
+		//driver.findElement(By.name("oneway")).click();
+		//driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[1]/a")).click();
+		System.out.println(driver.getTitle());
+	
+	
+
+	}
+
+}
